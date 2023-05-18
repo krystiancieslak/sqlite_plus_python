f191aae4cb652864705f230022856246b8ce8a07
Krystian <krystian.cieslak.kontakt@gmail.com>
2023-05-18 16:11:40 +0200

    dockerfile and code changes improving database creation

--- database_connexion.py
+++ database_connexion.py
@@ -1,44 +1,32 @@
 import os
 import sqlite3
 
-con = None
-cur = None
+database_dir = "database"
+database_file = f"{database_dir}/testDB.db"
 
 def check_database():
-    global con, cur
-    if not os.path.exists("testDB.db"):
-        con = sqlite3.connect("testDB.db")
+    if not os.path.exists(database_file):
+        # Creating new database
+        os.makedirs(database_dir, exist_ok=True)
+        con = sqlite3.connect(database_file)
         cur = con.cursor()
-        creating_table()
-        inserting_values()
+        creating_table(cur)
+        inserting_values(cur, con)
+        cur.close()
+        con.close()
+        print("New database has been created.")
     else:
-        con = sqlite3.connect("testDB.db")
-        cur = con.cursor()
-        if not table_exists("books"):
-            creating_table()
-            inserting_values()
+        print("Database already exists.")
 
-def table_exists(table_name):
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", (table_name,))
-    result = cur.fetchone()
-    return result is not None
+def creating_table(cur):
+    cur.execute("CREATE TABLE IF NOT EXISTS books(id, title, author)")
+    print("Creating table.")
 
-def creating_table():
-    try:
-        cur.execute("CREATE TABLE books(id, title, author)")
-        print("Table 'books' has been created.")
-    except sqlite3.OperationalError:
-        print("Table 'books' already exists.")
-
-def inserting_values():
-    cur.execute("""
-    INSERT INTO books VALUES
-        (1, 'Lord of the rings', 'J.R.R. Tolkien'),
-        (2, 'Harry Potter and the goblet of fire', 'J.K. Rowling'),
-        (3, 'I Heard You Paint Houses', 'Charles Brandt'),
-        (4, 'Before coffee gets cold', 'Toshikazu Kawaguchi')
-    """)
+def inserting_values(cur, con):
+    cur.execute("INSERT INTO books VALUES (1, 'Lord of the rings', 'J.R.R. Tolkien')")
+    cur.execute("INSERT INTO books VALUES (2, 'Harry Potter and the goblet of fire', 'J.K. Rowling')")
     con.commit()
+    print("Inserted values into table.")
 
 def main():
     check_database()
